0ace04b6b80fd2591558566a1bb11c2b122fa83f
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-07-13 22:52:48 -1200

    new react Practice project... 14th July

---
+++ AirBnB/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Head from './head'
+function App() {
+ return(
+    <div>
+        <Head />
+    </div>
+ )
+}
+
+export default App
---
+++ AirBnB/src/head.jsx
@@ -0,0 +1,11 @@
+import react from 'react'
+ 
+export default function head(){
+    return(
+        <div>
+            <nav>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMdtnSHQMwg8DYJnfLyUuUyZmXA6LFCJLXRyIUQ9EA&s" alt="" className='nav-img' />
+            </nav>
+        </div>
+    )
+}
