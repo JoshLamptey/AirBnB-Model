3e74c17d7e530c6cf21260f8d285b1f619febb27
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-07-16 20:09:46 -1200

    17th July commit

--- AirBnB/src/hero.jsx
+++ AirBnB/src/hero.jsx
@@ -2,8 +2,25 @@ import React from "react";
 import image from './images/Group 77.png'
 
 export default function Hero(){
+    const name = "Josh🤍 "
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay 
+
+    if (hours< 12) {
+        timeOfDay = "morning"
+    }else if ( hours > 12 && hours <17) {
+        timeOfDay = "afternoon"
+    }else if (hours>17 && hours <21) {
+        timeOfDay = "evening"
+    }else{
+        timeOfDay = "night"
+    }
     return (
+        
         <section className="hero">
+            <h1>Good {timeOfDay} {name}!</h1>
+
             <img src={image}className="hero-img"/>
             <h1 className="hero-title">Online Experiences</h1>
             <p className="hero-text">
