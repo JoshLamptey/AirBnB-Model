7e1149ca8d64bf252886dacb298372fbcafce20b
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-07-28 23:32:19 -1200

    third

--- AirBnB/src/Body.jsx
+++ AirBnB/src/Body.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import Star from './images/Star 1star.png'
 
-export default function Body({img,name,price,country,rating,reviewCount}){
+export default function Body(props){
    
 
     return(
         <section>
             <div className="Xperience1">
-                <img src= {img} alt="" className="X-img1" />
+                <img src= {props.img} alt="" className="X-img1" />
                 <section>
                     <div className="rating">
                     <img src= {Star}alt="" className="star" />
-                    <span className="gray" >{rating}</span>
-                    <span className="gray">({reviewCount})• </span>
-                    <span>{country}</span>
+                    <span className="gray" >{props.rating}</span>
+                    <span className="gray">({props.reviewCount})• </span>
+                    <span>{props.country}</span>
                     </div>
-                    <p>{name}
-                        <br />
-                    <span className="bold">{price}</span> per person</p>
+                    <p>
+                        <h3>{props.title}</h3>
+                        {props.description}
+                        
+                    <span className="bold">{props.price}</span> per person</p>
                 </section>
             </div>
         </section> 
