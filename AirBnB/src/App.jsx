ef247b72726886555b93f06d168ecf8d3876a172
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-07-29 02:20:05 -1200

    final rendition

--- AirBnB/src/App.jsx
+++ AirBnB/src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
-import X from './images/image 12.png'
-import Y from './images/mountain-bike 1.png'
-import V from './images/wedding-photography 1.png'
+
 import Data from './Data'
 
 import Head from './head'
@@ -13,10 +11,9 @@ function App() {
         return <Body 
         id ={element.id}
         title ={element.title}
-        img = {element.img}
+        img = {element.coverImg}
         description ={element.description}
         price ={element.price}
-        coverImg ={element.coverImg}
         rating ={element.stats.rating}
         reviewCount={element.stats.reviewCount}
         location ={element.location}
@@ -30,33 +27,7 @@ function App() {
         <Head />
         <Hero/>
         <div className="container">
-        <Body 
-       // img ={X}
-       // name ="Life Lessons with Annie Zaferes"
-       // price ="From 136$/"
-      //  rating ={4.7}
-      //  reviewCount ={6}
-      //  country="USA"
-
-        />
-      //  <Body
-      //  img ={Y}
-      //  name ="Group Mountiain Biking experience"
-      //  price ="From 125$/"
-     //   rating ={4.5}
-      //  reviewCount ={6}
-      //  country="Canada"
-
-         />
-        <Body 
-      //  img ={V}
-      //  name ="Learn wedding photography"
-     //   price ="From 136$/"
-      //  rating ={4.1}
-        //reviewCount ={6}
-       // country="Ireland"
-
-        />
+    {DataElements}
 </div>
     </div>
  )
